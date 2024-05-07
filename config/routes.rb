0178b267a075b13d460f12baf047a47df9d55f56
latmannen <163900633+latmannen@users.noreply.github.com>
2024-05-07 18:46:27 +0000

    created new photo form

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
 
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
+  post("/insert_photo", { :controller => "photos", :action => "create"})
+  post("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
 end
