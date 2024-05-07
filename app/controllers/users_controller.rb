a706d04dd46f3283f15ef5517f789aceb3eca623
latmannen <163900633+latmannen@users.noreply.github.com>
2024-05-07 17:14:30 +0000

    user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({ :username => :asc})
     render({ :template => "user_templates/index"})
   end
+
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+    
+    matching_usernames = User.where({ :username => url_username})
+    
+    @the_user = matching_usernames.first
+
+      render({ :template => "user_templates/show"})
+  end
+
 end
